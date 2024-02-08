c4f5baebcd95a156a67e29f927be7b0b24d1093b
Liliana Gallegos <43852154+lilianaruva@users.noreply.github.com>
2024-02-08 12:30:39 -0600

    create functions of productsStore

--- src/store/productsStore.js
+++ src/store/productsStore.js
@@ -3,6 +3,13 @@ import dummy from '../dummy.json'
 
 export const useProductsStore = create((set) => ({
   productList: dummy,
+  auxProductList: dummy,
   addProduct: (product) => set((state) => ({ productList: [...state.productList, product] })),
+  sortProductsByName: () => set((state) => ({
+    productList: [...state.productList.sort((a, b) => a.name.localeCompare(b.name))]
+  })),
+  resetProductList: () => set((state) => ({
+    productList: state.auxProductList
+  }))
 }))
 
