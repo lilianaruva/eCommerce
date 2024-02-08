749c6ed329877e17737a41fa1545c7b7e112c06e
Liliana Gallegos <43852154+lilianaruva@users.noreply.github.com>
2024-02-08 15:50:13 -0600

    delete product to cart by id

--- src/components/cardShopping.jsx
+++ src/components/cardShopping.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useShoppingStore } from '../store/shoppingStore';
 
 export default function CardShopping({data}) {
+  const { removeProductInToCart } = useShoppingStore();
   return (
     <div className='flex gap-4 shadow-md rounded-md bg-white'>
         <img className='rounded-lg w-[120px] h-[120px]' alt='productId' src={data.image} />
         <div>
             <p>{data.label}</p>
             <p>${data.price}</p>
+            <p onClick={() => removeProductInToCart(data.id)} className='hover:underline hover:cursor-pointer text-red-500'>Delete</p>
         </div>
     </div>
   )
