2debd08005e0fa877f995839d0859c6e9d05b076
Liliana Gallegos <43852154+lilianaruva@users.noreply.github.com>
2024-02-08 12:58:28 -0600

    redirect to product id

--- src/components/cardProduct.jsx
+++ src/components/cardProduct.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
-export default function CardProduct() {
+export default function CardProduct({data}) {
+  const navigate = useNavigate();
   return (
-    <div className='hover:cursor-pointer bg-white rounded-md hover:scale-105 transition-all shadow-md'>
+    <div onClick={() => navigate(`/products/${data.id}`)} key={data.id} className='hover:cursor-pointer bg-white rounded-md hover:scale-105 transition-all shadow-md'>
         <img className='rounded-lg w-full h-[200px] object-cover' alt='productId' src='https://http2.mlstatic.com/nike-hombre-tenis-D_NQ_NP_437125-MCO25390148738_022017-F.jpg' />
         <div className='flex flex-col gap-4 p-8'>
-            <p className='font-semibold text-lg'>Product</p>
-            <p>Price</p>
+            <p className='font-semibold text-lg'>{data.label}</p>
+            <p>${data.price}</p>
         </div>
     </div>
   )
