c299b0cc3e1cc3426023aa0714d7face851f4a09
Liliana Gallegos <43852154+lilianaruva@users.noreply.github.com>
2024-02-08 13:03:33 -0600

    go back button

--- src/pages/productDetails/index.jsx
+++ src/pages/productDetails/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from '../../components/navbar'
+import { useNavigate } from 'react-router-dom';
 
 export default function ProductDetails() {
+  const navigate = useNavigate();
   return (
     <>
     <Navbar />
+    <div className='my-10 mx-10 sm:mx-40'>
+      <button onClick={() => navigate(-1)} className='hover:underline'>Go Back</button>
+    </div>
     <div className='grid grid-cols-1 sm:grid-cols-2 gap-8 my-10 mx-10 sm:mx-40'>
       <img className='rounded-lg w-full max-h-100' alt='productId' src='https://http2.mlstatic.com/nike-hombre-tenis-D_NQ_NP_437125-MCO25390148738_022017-F.jpg' />
       <div className='flex flex-col gap-4'>
