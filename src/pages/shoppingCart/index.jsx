f6c44a2e41edbb1ddc1ab506b19ccf80b940a9b2
Liliana Gallegos <43852154+lilianaruva@users.noreply.github.com>
2024-02-08 13:40:20 -0600

    get info by shoppingCart store

--- src/pages/shoppingCart/index.jsx
+++ src/pages/shoppingCart/index.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import CardShopping from '../../components/cardShopping'
 import Navbar from '../../components/navbar'
+import { useShoppingStore } from '../../store/shoppingStore';
 export default function ShoppingCart() {
+  const { shopList } = useShoppingStore();
+  const totalPrice = shopList.reduce((total, product) => total + product.price, 0);
+
+
   return (
     <>
     <Navbar />
-    <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 my-10 mx-10 sm:mx-40'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-16 my-10 mx-10 sm:mx-40'>
       <div className='flex flex-col gap-4 order-last sm:order-first'>
-        <p className='text-xl font-bold'>Products</p>
+        <p className='text-xl font-bold'>{shopList?.length ?? 0} Products</p>
         <div className='flex flex-col gap-4 max-h-96 overflow-y-scroll'>
-          <CardShopping />
-          <CardShopping />
-          <CardShopping />
-          <CardShopping />
-          <CardShopping />
+          {
+            shopList?.map(product => <CardShopping key={product.id} data={product}/>)
+          }
         </div>
       </div>
       <div className='flex flex-col gap-4'>
         <p className='text-3xl font-bold'>Estimated Total</p>
-        <p className='text-xl'>$Price</p>
+        <p className='text-xl'>${totalPrice}</p>
         <button className='w-full max-w-96 bg-indigo-500 hover:bg-indigo-400 ease-in duration-200 rounded-md shadow-lg shadow-indigo-500/50 text-white p-3'>Checkout</button>
       </div>
     </div>
