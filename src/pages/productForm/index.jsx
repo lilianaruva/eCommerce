884dfa39dd037fd3ad5906620f93b1f6b3c8ec9f
Liliana Gallegos <43852154+lilianaruva@users.noreply.github.com>
2024-02-08 15:00:28 -0600

    add new product into list

--- src/pages/productForm/index.jsx
+++ src/pages/productForm/index.jsx
@@ -1,13 +1,60 @@
-import React from 'react'
-import Navbar from '../../components/navbar'
+import React, { useState } from 'react';
+import Navbar from '../../components/navbar';
+import TextField from '@mui/material/TextField';
+import { FormControl, InputAdornment, InputLabel, OutlinedInput } from '@mui/material';
+import { useProductsStore } from '../../store/productsStore';
+import { toast } from 'react-toastify';
 
 export default function ProductForm() {
+  const [productName, setProductName] = useState('');
+  const [productPrice, setProductPrice] = useState('');
+  const [productImage, setProductImage] = useState(null);
+  const { addProduct } = useProductsStore();
+
+  const handleSaveProduct = () => {
+    if (productName && productPrice && productImage) {
+      const newProduct = {
+        id: Math.random().toString(36),
+        label: productName,
+        price: parseFloat(productPrice),
+        image: URL.createObjectURL(productImage),
+      };
+      addProduct(newProduct);
+      setProductName('');
+      setProductPrice('');
+      setProductImage(null);
+      toast.success('Product Added')
+    } else {
+      toast.error('Please fill in all fields.');
+    }
+  };
+
   return (
     <>
       <Navbar />
-      <div>
-        hola
+      <div className='gap-4 sm:gap-8 my-10 mx-10 sm:mx-40 w-1/3 grid grid-cols-1'>
+        <p className='text-2xl font-semibold'>Add New Product 🛒</p>
+        <TextField
+          type='text'
+          value={productName}
+          onChange={(e) => setProductName(e.target.value)}
+          id="label"
+          label="Product Name"
+        />
+        <FormControl fullWidth sx={{ m: 1 }}>
+          <InputLabel htmlFor="price">Amount</InputLabel>
+          <OutlinedInput
+            type='number'
+            value={productPrice}
+            onChange={(e) => setProductPrice(e.target.value)}
+            id="price"
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            label="Amount"
+          />
+        </FormControl>
+        <input type="file" onChange={(e) => setProductImage(e.target.files[0])} />
+        <button onClick={handleSaveProduct} className='w-full bg-indigo-500 hover:bg-indigo-400 ease-in duration-200 rounded-md shadow-lg shadow-indigo-500/50 text-white p-3'>Save Product</button>
       </div>
     </>
-  )
+  );
 }
--- src/store/productsStore.js
+++ src/store/productsStore.js
@@ -4,7 +4,10 @@ import dummy from '../dummy.json'
 export const useProductsStore = create((set) => ({
   productList: dummy,
   auxProductList: dummy,
-  addProduct: (product) => set((state) => ({ productList: [...state.productList, product] })),
+  addProduct: (product) => set((state) => ({ 
+    productList: [...state.productList, product],
+    auxProductList: [...state.productList, product] 
+  })),
   sortProductsByName: () => set((state) => ({
     productList: [...state.productList.sort((a, b) => a.label.localeCompare(b.label))]
   })),
